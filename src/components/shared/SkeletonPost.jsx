9800ffec69be69c4894a0cb0057dd0577499aae6
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-11 17:07:54 +0530

    Modify the skeleton

--- src/components/shared/SkeletonPost.jsx
+++ src/components/shared/SkeletonPost.jsx
@@ -1,5 +1,11 @@
-export default function SkeletonPost() {
+const SkeletonPost = ({ height = "h-60" }) => {
   return (
-    <div className="bg-gray-200 dark:bg-slate-800 animate-pulse rounded-md h-60 mb-4 w-full max-w-xl mx-auto"></div>
+    <div 
+      className={`bg-purple-100 dark:bg-slate-800 animate-pulse rounded-md ${height} mb-4 w-full max-w-xl mx-auto`}
+      role="status"
+      aria-label="Loading post"
+    ></div>
   );
-}
+};
+
+export default SkeletonPost;
