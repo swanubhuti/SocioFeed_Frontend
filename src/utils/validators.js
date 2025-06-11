c8567434fb22f4442c137b8fec7c6bd8a4d6c131
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-11 16:30:14 +0530

    [Fix]: Added unique usernsme validation

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,11 +1,38 @@
 import * as yup from 'yup';
+import { checkUsernameUniqueness } from '../features/auth/authAPI';
 
 export const registerSchema = yup.object().shape({
 	username: yup
 		.string()
 		.required('Username is required')
 		.min(2, 'Must be at least 2 characters')
-		.matches(/^[a-zA-Z0-9_]+$/, 'Alphanumeric only'),
+		.matches(
+			/^[a-zA-Z0-9_]+$/,
+			'Username can only contain letters, numbers, and underscores'
+		) //Alphanumeric
+		.test(
+			'check-username-unique',
+			'This username is already taken',
+			async function (value) {
+				if (!value || value.length < 3) {
+					return true;
+				}
+				try {
+					const data = await checkUsernameUniqueness(value);
+					if (!data.isUnique) {
+						return this.createError({
+							message: data.message || 'This username is already taken',
+						});
+					}
+					return true;
+				} catch (error) {
+					const errorMessage =
+						error.response?.data?.message ||
+						'Error checking username availability';
+					return this.createError({ message: errorMessage });
+				}
+			}
+		),
 	email: yup.string().email('Invalid email').required('Email is required'),
 	password: yup
 		.string()
