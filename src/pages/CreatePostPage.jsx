545140d82dcf35715db4a4d82e4fccbeba94a458
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-11 17:43:08 +0530

    Modigy css

--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -6,7 +6,6 @@ const CreatePostPage = () => {
     <div className="flex min-h-screen bg-purple-50 dark:bg-slate-900 transition-colors">
       <Sidebar />
       <main className="flex-1 p-4 sm:p-6 max-w-screen-md lg:max-w-2xl mx-auto w-full">
-
         <CreatePost />
       </main>
     </div>
