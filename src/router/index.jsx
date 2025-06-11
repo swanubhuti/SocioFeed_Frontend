265a8651582eae9326d9250bda512d20340dc127
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-11 16:55:56 +0530

    [Fix] Added routes for chat and saved page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,3 @@
-// src/router/index.js
-
 import { Routes, Route } from 'react-router-dom';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
@@ -7,46 +5,41 @@ import VerifyAccountt from '../pages/VerifyAccountt';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
 import Home from '../pages/Home';
-import ProfilePage from '../pages/ProfilePage'; // Ensure this path is correct, it might be '../pages/ProfilePage'
+import ProfilePage from '../pages/ProfilePage'; 
 import SearchPage from '../pages/SearchPage';
-import AuthProvider from '../api/AuthProvider'; // Your AuthProvider is correctly imported
-
-// Import the new ProtectedRoute component
-import ProtectedRoute from '../components/ProtectedRoute'; // <--- ADD THIS IMPORT
+import AuthProvider from '../api/AuthProvider'; 
+import PostDetail from '../pages/PostDetail';
+import ProtectedRoute from '../components/ProtectedRoute'; 
 import CreatePostPage from '../pages/CreatePostPage';
 import PageNotFound from '../pages/NotFound';
-
+import ChatPage from '../pages/ChatPage';
+import SavedPostsPage from '../pages/SavedPostPage';
 
 export default function AppRouter() {
-  return (
-    <>
-      <AuthProvider> {/* AuthProvider correctly wraps all routes to provide auth context */}
-        <Routes>
-          {/* Public Routes - Accessible to everyone */}
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/verify-account" element={<VerifyAccountt />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
-          
+	return (
+		<>
+			<AuthProvider>
+				<Routes>
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/verify-account" element={<VerifyAccountt />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
+					<Route path="/reset-password/:token" element={<ResetPassword />} />
 
-          {/* Protected Routes - Only accessible if authenticated */}
-          {/* Use the <Route element={<ProtectedRoute />}> to wrap your protected routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Home />} /> {/* Home is usually protected */}
-            <Route path="/profile/:username" element={<ProfilePage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path= "/new-post" element = {<CreatePostPage />} />
-            {/* Add any other routes that should only be accessible when logged in */}
-          </Route>
+					<Route element={<ProtectedRoute />}>
+						<Route path="/" element={<Home />} />{' '}
+						<Route path="/profile/:username" element={<ProfilePage />} />
+						<Route path="/search" element={<SearchPage />} />
+						<Route path="/new-post" element={<CreatePostPage />} />
+						<Route path="/post/:postId" element={<PostDetail />} />
+						<Route path="/chat" element={<ChatPage />} />
+						<Route path="/saved" element={<SavedPostsPage />} />
 
-          {/* You might also want a catch-all for 404 pages */}
-          {/* <Route path="*" element={<NotFoundPage />} /> */}
-          
-          <Route path="*" element={<PageNotFound />} /> 
-        </Routes>
-      </AuthProvider>
-      
-    </>
-  );
+					</Route>
+
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</AuthProvider>
+		</>
+	);
 }
