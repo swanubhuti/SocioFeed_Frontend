4d7bbde6bd5ec280efd4b67cb6ad7364c9469272
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-11 16:41:22 +0530

    [Fix]: Wrap App inside Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store,persistor } from './app/store';
+import { store } from './app/store';
 import ThemeProviderWrapper from './theme/ThemeProviderWrapper';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
-   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ThemeProviderWrapper>
-              <Toaster position="top-right" reverseOrder={false} />
-         <PersistGate loading={null} persistor={persistor}>
-			<App />
-		</PersistGate>
-        </ThemeProviderWrapper>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
+	<StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<ThemeProviderWrapper>
+					<Toaster position="top-right" reverseOrder={false} />
+					<App />
+				</ThemeProviderWrapper>
+			</BrowserRouter>
+		</Provider>
+	</StrictMode>
 );
