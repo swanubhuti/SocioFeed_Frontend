4456fddcd8b2cfeb39884848d4df69363384b0a2
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-11 17:44:39 +0530

    Modify css

--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -5,6 +5,7 @@ const CreatePostPage = () => {
   return (
     <div className="flex min-h-screen bg-purple-50 dark:bg-slate-900 transition-colors">
       <Sidebar />
+      
       <main className="flex-1 p-4 sm:p-6 max-w-screen-md lg:max-w-2xl mx-auto w-full">
         <CreatePost />
       </main>
